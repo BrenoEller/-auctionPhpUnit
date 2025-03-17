b4a5287461dd5b3f1453216144dfe1eaaf3d310d
Breno <brenoeller@hotmail.com>
2025-03-17 19:04:05 -0300

    5 lances per user

--- src/Model/Leilao.php
+++ src/Model/Leilao.php
@@ -20,6 +20,11 @@ class Leilao
         if (!empty($this->lances) && $this->ehDoUltimoUsuario($lance)) {
             return;
         }
+        $totalLancesUsuario = $this->quantidadeLancesPorUsuario($lance->getUsuario());
+        if ($totalLancesUsuario >= 5) {
+            return;
+        }
+
         $this->lances[] = $lance;
     }
 
@@ -35,5 +40,24 @@ class Leilao
     {
         $ultimoLance = $this->lances[count($this->lances) - 1];
         return $lance->getUsuario() == $ultimoLance->getUsuario();
+    }
+
+    /**
+     * @param Usuario $usuario
+     * @return Lance|mixed
+     */
+    private function quantidadeLancesPorUsuario(Usuario $usuario): int
+    {
+        $totalLancesUsuario = array_reduce(
+            $this->lances,
+            function (int $totalAcumulado, Lance $lanceAtual) use ($usuario) {
+                if ($lanceAtual->getUsuario() == $usuario) {
+                    return $totalAcumulado + 1;
+                }
+                return $totalAcumulado;
+            },
+            0
+        );
+        return $totalLancesUsuario;
     }
 }
